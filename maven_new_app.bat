f7df267f8721363aabc7256b1f8a4e3d6758f4a1
manuel campos <manuel.campos@inycom.es>
2018-05-08 15:15:55 +0200

    Added servlet Technology

---
+++ maven_new_app.bat
@@ -0,0 +1,5 @@
+::mvn archetype:generate -DgroupId=com.mycompany.app -DartifactId=my-app -DarchetypeArtifactId=maven-archetype-quickstart -DinteractiveMode=false
+
+mvn archetype:generate -DgroupId=com.test.app -DartifactId=mvn_testapp -DarchetypeArtifactId=maven-archetype-quickstart -DinteractiveMode=false
+
+@CMD
